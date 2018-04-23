93ffda5524baf2b9f1b55dd601051185bc6424ab
Lucas Tavares <lucastavaresdev@gmail.com>
2018-04-23 11:22:53 -0300

    criação da primeira tarefa com sass

--- projeto/gulpfile.js
+++ projeto/gulpfile.js
@@ -1,1 +1,10 @@
-var gulp = require('gulp');
+var gulp = require('gulp')
+    , sass = require('gulp-sass');
+
+//nome da tarefa e o que vai execultar
+gulp.task('sass', function () {
+    gulp.src('./src/sass/**/*.scss')//origem; qualquer pasta e qualquer arquivo
+        .pipe(sass())//tratamento com o gulp-sass
+        .pipe(gulp.dest('./src/css/'));//destino; o pipe faz o tratamento
+});
+
