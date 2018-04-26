e2e380d6fe5fbd18824e8ba0c406c22c15b4b5ed
Lucas Tavares <lucastavaresdev@gmail.com>
2018-04-26 11:31:04 -0300

    animação finalizada sem refatorar

--- projeto/gulpfile.js
+++ projeto/gulpfile.js
@@ -1,13 +1,12 @@
-var gulp = require('gulp')
-    , sass = require('gulp-sass');
+var gulp  = require('gulp')
+,sass = require('gulp-sass');
 
-//nome da tarefa e o que vai execultar
-gulp.task('sass', function () {
-    gulp.src('./src/sass/**/*.scss')//origem; qualquer pasta e qualquer arquivo
-        .pipe(sass())//tratamento com o gulp-sass
-        .pipe(gulp.dest('./src/css/'));//destino; o pipe faz o tratamento
-});
+gulp.task('sass', function(){
+gulp.src('./src/sass/**/*.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('./src/css/'));
+})
 
 gulp.task('listen', function(){
-    gulp.watch('./src/sass/**/*.scss', ['sass']);
+gulp.watch('./src/sass/**/*.scss',['sass'])
 })
