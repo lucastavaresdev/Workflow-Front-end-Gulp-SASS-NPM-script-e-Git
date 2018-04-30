94855db18040e028edc8ab7c6a610cb3499de6a5
Lucas Tavares <lucastavaresdev@gmail.com>
2018-04-30 16:04:40 -0300

    live serve

--- projeto/gulpfile.js
+++ projeto/gulpfile.js
@@ -1,5 +1,6 @@
 var gulp  = require('gulp')
-,sass = require('gulp-sass');
+,sass = require('gulp-sass'),
+browserSync = require('browser-sync');
 
 gulp.task('sass', function(){
 gulp.src('./src/sass/**/*.scss')
@@ -9,4 +10,12 @@ gulp.src('./src/sass/**/*.scss')
 
 gulp.task('listen', function(){
 gulp.watch('./src/sass/**/*.scss',['sass'])
+})
+
+gulp.task('server', function(){
+    browserSync.init({
+        server:{
+            baseDir: 'src'
+        }
+    })
 })
