020852585b8589f7e6ea9290c413bedaca5c7cc5
Lucas Tavares <lucastavaresdev@gmail.com>
2018-05-02 10:42:54 -0300

    menu animação nao finalizado

---
+++ projeto/src/javascript/menu.js
@@ -0,0 +1,28 @@
+;(function name(params) {
+    'use strict';
+
+    var btn = document.querySelector('.header-nav__hamburgger');
+    var html = document.querySelector('html');
+    var menuOpened = false;
+
+    btn.addEventListener('click', toggleMenu);
+
+    function toggleMenu(e){
+        if(menuOpened){
+            closeMenu()
+        }else{
+            openMenu();
+        }
+    }
+
+    function closeMenu(){
+        menuOpened = false;
+        html.classList.remove('menu-opened')
+    }
+    function openMenu(){
+        menuOpened = true;
+
+        html.classList.add('menu-opened')
+    }
+
+})();
