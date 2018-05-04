8802b0a3a52e83dc3f1fe05559232fd95ca33b8c
Lucas Tavares <lucastavaresdev@gmail.com>
2018-05-04 14:08:31 -0300

    task para limpar a pasta antes de copiar

--- projeto/gulpfile.js
+++ projeto/gulpfile.js
@@ -1,30 +1,36 @@
-var gulp  = require('gulp')
-,sass = require('gulp-sass')
-, include = require('gulp-file-include');
-browserSync = require('browser-sync');
+var gulp = require('gulp')
+    , sass = require('gulp-sass')
+    , include = require('gulp-file-include')
+    , clean = require('gulp-clean')
+    , browserSync = require('browser-sync')
 
-gulp.task('sass', function(){
-gulp.src('./src/sass/**/*.scss')
-    .pipe(sass())
-    .pipe(gulp.dest('./src/css/'));
-})
-
-gulp.task('copy', function(){
-    gulp.src(['src/components/**/*' , 'src/css/**/*' , 'src/javascript/**/*', 'src/img/**/*'], {"base": "src"})//o base mantem a estrutura
-    .pipe(gulp.dest('dist'))
+gulp.task('clean', function(){
+  return gulp.src('dist')
+    .pipe(clean());
 })
 
 
+gulp.task('copy',['clean'] ,function () {
+    gulp.src(['src/components/**/*', 'src/css/**/*', 'src/javascript/**/*', 'src/img/**/*'], { "base": "src" })//o base mantem a estrutura
+        .pipe(gulp.dest('dist'))
+})
 
-gulp.task('html',function(){
-    gulp.src('./src/index.html')
+gulp.task('sass', function () {
+    gulp.src('./src/sass/**/*.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('./src/css/'));
+})
+
+
+gulp.task('html', function () {
+   return gulp.src('./src/index.html')
         .pipe(include())
         .pipe(gulp.dest('./dist/'))
 })
 
-gulp.task('serve', function(){
+gulp.task('serve',['html'] ,function () {
     browserSync.init({
-        server:{
+        server: {
             baseDir: 'dist'
         }
     })
