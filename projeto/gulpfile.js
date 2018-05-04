b9249fa8476dceb03b1fd7fb1c6db8994030fbf7
Lucas Tavares <lucastavaresdev@gmail.com>
2018-05-04 15:25:44 -0300

    aproveitando loop de codigo

--- projeto/gulpfile.js
+++ projeto/gulpfile.js
@@ -4,37 +4,46 @@ var gulp = require('gulp')
     , clean = require('gulp-clean')
     , browserSync = require('browser-sync')
 
-gulp.task('clean', function(){
-  return gulp.src('dist')
-    .pipe(clean());
+gulp.task('clean', function () {
+    return gulp.src('dist')
+        .pipe(clean());
 })
 
 
-gulp.task('copy',['clean'] ,function () {
-    gulp.src(['src/components/**/*', 'src/css/**/*', 'src/javascript/**/*', 'src/img/**/*'], { "base": "src" })//o base mantem a estrutura
+gulp.task('copy', ['clean'], function () {
+    gulp.src([
+        'src/components/**/*',
+        'src/css/**/*',
+        'src/javascript/**/*',
+        'src/img/**/*',
+        'src/index.html',
+    ], { "base": "src" })//o base mantem a estrutura
         .pipe(gulp.dest('dist'))
 })
+
+
+
 
 gulp.task('sass', function () {
     gulp.src('./src/sass/**/*.scss')
         .pipe(sass())
-        .pipe(gulp.dest('./src/css/'));
+        .pipe(gulp.dest('./dist/css/'));
 })
-
-
 gulp.task('html', function () {
-   return gulp.src('./src/index.html')
+    return gulp.src('./src/index.html')
         .pipe(include())
         .pipe(gulp.dest('./dist/'))
 })
 
-gulp.task('serve',['html'] ,function () {
+gulp.task('serve', ['html'], function () {
     browserSync.init({
         server: {
             baseDir: 'dist'
         }
     })
-    gulp.watch('./src/**/*').on('change', browserSync.reload)
+
+    gulp.watch('./dist/**/*').on('change', browserSync.reload)
 
     gulp.watch('./src/sass/**/*.scss', ['sass'])
+    gulp.watch('./src/**/*.html', ['html'])
 })
