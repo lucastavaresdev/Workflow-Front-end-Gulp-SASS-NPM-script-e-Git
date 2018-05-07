ebe0ba81bc092c65bfefe975b2b3b5c034b924d6
Lucas Tavares <lucastavaresdev@gmail.com>
2018-05-07 16:05:01 -0300

    plugin para diminuir as imagem

--- projeto/gulpfile.js
+++ projeto/gulpfile.js
@@ -4,57 +4,65 @@ var gulp = require('gulp')
     , clean = require('gulp-clean')
     , autoprefixer = require('gulp-autoprefixer')
     , postcss = require('gulp-postcss')
+    , imagemin = require('gulp-imagemin')
     , browserSync = require('browser-sync')
 
-gulp.task('clean', function () {
-    return gulp.src('dist')
-        .pipe(clean());
-})
+    gulp.task('clean', function(){
+        return gulp.src('dist')
+            .pipe(clean());
+    })
+    
 
 
 gulp.task('copy', ['clean'], function () {
     gulp.src([
         'src/components/**/*',
-        'src/css/**/*',
         'src/javascript/**/*',
-        'src/img/**/*',
         'src/index.html',
     ], { "base": "src" })//o base mantem a estrutura
         .pipe(gulp.dest('dist'))
 })
 
 
-
-
-gulp.task('sass', function () {
-    gulp.src('./src/sass/**/*.scss')
+gulp.task('sass', function(){
+    return gulp.src('./src/sass/**/*.scss')
         .pipe(sass())
         .pipe(autoprefixer())
         .pipe(gulp.dest('./dist/css/'));
 })
-gulp.task('html', function () {
-    return gulp.src('./src/**/*.html')
+
+gulp.task('html', function(){
+    return gulp.src([
+            './src/**/*.html',
+            '!src/inc/**'
+        ])
         .pipe(include())
         .pipe(gulp.dest('./dist/'))
 })
 
 
 
-
-
-gulp.task('uncss',['html'], function () {
-    return gulp.src('/dist/components/**/*.css')
+gulp.task('uncss', ['html'] , function () {
+    return gulp.src('./dist/components/**/*.css')
         .pipe(postcss())
         .pipe(gulp.dest('./dist/components/'));
 });
 
 
-gulp.task('serve', ['uncss'], function () {
+gulp.task('imagemin', function(){
+    return gulp.src('./src/img/**/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('./dist/imagens/'))
+});
+
+
+
+gulp.task('serve', ['uncss', 'imagemin', 'sass', 'copy'], function () {
     browserSync.init({
         server: {
             baseDir: 'dist'
         }
-    })
+});
 
     gulp.watch('./dist/**/*').on('change', browserSync.reload)
 
